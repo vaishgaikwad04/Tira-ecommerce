0a582393156d77c2221ebc5f686b5b4fb19d0ced
vaishnavi gaikwad <vaishgaikwad472@gmail.com>
2026-03-22 09:51:27 +0530

    chnage mongo atals ui

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectedToDb = async () => {
     try{
-        await mongoose.connect(process.env.URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('mongodb connected');
 
     }catch(error){
